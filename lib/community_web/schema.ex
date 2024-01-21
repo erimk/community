bbe4ad86e95e02063f7d072f72871d4e4c5034e4
Erica M. Kanashiro <erimk@live.com>
2024-01-21 17:40:00 -0300

    barebone of query schema

---
+++ lib/community_web/schema.ex
@@ -0,0 +1,12 @@
+defmodule CommunityWeb.Schema do
+  #use Absinthe.Schema
+
+  #alias CommunityWeb.NewsResolver
+
+  #query do
+    # this is the query entry point to our app
+
+    ##@desc "Get all links"
+    ##field :all_links, non_null(list_of(non_null(:link)))
+  #end
+end
--- mix.exs
+++ mix.exs
@@ -61,7 +61,7 @@ defmodule Community.MixProject do
       setup: ["deps.get", "ecto.setup"],
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
-      test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"]
+      test: ["ecto.drop", "ecto.create --quiet", "ecto.migrate --quiet", "test"]
     ]
   end
 end
